1c5eccfb77a98666b17d30bacac5b09daf1a5b87
mmaracic <marijo.maracic@gmail.com>
2026-07-29 02:48:54 +0200

    Added ADC measurement of temperature and lighting

--- temp-light-wifi-python/README.md
+++ temp-light-wifi-python/README.md
@@ -1,6 +1,6 @@
 # README
 
-MicroPython project to load environemnt variables from a JSON file and connect to a Wi-Fi network using those variables, measure emperature and light levels using ADC, and send the data to a server using HTTP requests with Json payload. The project is designed to run on a Raspberry Pi Pico W microcontroller.
+MicroPython project to load environemnt variables from a JSON file and connect to a Wi-Fi network using those variables, measure temperature and light levels using ADC, and send the data to a server using HTTP requests with Json payload. The project is designed to run on a Raspberry Pi Pico W microcontroller.
 
 
 ## Setup
@@ -15,7 +15,8 @@ For the project to work, you need to create a `local_settings.json` file in the 
     "AZURE_CODE": "your_azure_function_key",
     "CONTAINER_NAME": "your_container_name",
     "TOPIC_NAME": "your_topic_name",
-    "LIMIT": 1
+    "LIMIT": 1,
+    "SAMPLE_INTERVAL_SEC": 60
 }
 ```
 All three files `network.py`, `utils.py`, and `local_settings.json` are required for the project to work and need to be uploaded to the Pico.
@@ -30,3 +31,7 @@ https://pip-assets.raspberrypi.com/categories/686-raspberry-pi-pico-w/documents/
 
 Urequests library is used to send HTTP requests to the server. You can find more information about the urequests library and its usage in the official documentation:  
 https://makeblock-micropython-api.readthedocs.io/en/latest/public_library/Third-party-libraries/urequests.html
+
+Added ADC data. ADC conversion is 12-bit but converted to 16-bit number. The adc.read_u16() method is used to read the ADC value from three ADC channels: the internal temperature sensor and the external temperature and light sensors. The method returns a 16-bit integer value that represents the voltage level of the sensor. You can find more information about the ADC class and its methods in the official documentation:
+https://pip-assets.raspberrypi.com/categories/610-raspberry-pi-pico/documents/RP-008355-DS-1-raspberry-pi-pico-python-sdk.pdf
+https://docs.micropython.org/en/latest/library/machine.ADC.html
--- temp-light-wifi-python/network.py
+++ temp-light-wifi-python/network.py
@@ -1,25 +1,33 @@
 import json
 import time
 
+import machine
 import urequests
 from machine import Pin
 from utime import sleep
 
 import network
-from utils import get_env_variable, load_json_file, get_url_with_params
+from utils import get_env_variable, get_url_with_params, load_json_file
 
 env = load_json_file("local_settings.json")
 NETWORK_SSID = get_env_variable("NETWORK_SSID", env)
 PASSWORD = get_env_variable("PASSWORD", env)
+CONNECTION_RETRY_LIMIT = 20
 
 REQUEST_URL = get_env_variable("REQUEST_URL", env)
 AZURE_CODE = get_env_variable("AZURE_CODE", env)
 CONTAINER_NAME = get_env_variable("CONTAINER_NAME", env)
 TOPIC_NAME = get_env_variable("TOPIC_NAME", env)
 LIMIT = get_env_variable("LIMIT", env)
+SAMPLE_INTERVAL_SEC = get_env_variable("SAMPLE_INTERVAL_SEC", env)
+
+led = Pin("LED", Pin.OUT)
+adc_int_temp = machine.ADC(4)  # Connect to the internal temperature sensor
+adc_temp = machine.ADC(0)  # Connect to channel 0 (GP26)
+adc_light = machine.ADC(1)  # Connect to channel 1 (GP27)
+conversion_factor = 3.3 / (65535)
 
 print("Starting application...")
-led = Pin("LED", Pin.OUT)
 led.toggle()
 sleep(1)  # sleep 1sec
 led.toggle()
@@ -42,39 +50,63 @@ for net in networks:
 if networkAvailable:
     wlan.connect(NETWORK_SSID, PASSWORD)
     attempts = 0
-    while attempts < 15:
+    while attempts < CONNECTION_RETRY_LIMIT:
         status = wlan.status()
         if status == network.STAT_GOT_IP:
             break
         if status < 0:
             print(f"Connection error, status: {status}")
             break
-        print(f"Waiting to connect (attempt {attempts + 1}/15), status: {status}...")
+        print(f"Waiting to connect (attempt {attempts + 1}/{CONNECTION_RETRY_LIMIT}), status: {status}...")
         time.sleep(1)
         attempts += 1
     ip = wlan.ifconfig()[0]
     if wlan.status() == network.STAT_GOT_IP and ip != "0.0.0.0" and wlan.isconnected():
         print(f"Connected! IP: {ip}")
+        try:
+            while True:
+                led.toggle()
+                adc_int_temp_value = adc_int_temp.read_u16() * conversion_factor
+                int_temp_c = 27 - (adc_int_temp_value - 0.706) / 0.001721
+                adc_temp_value = adc_temp.read_u16() * conversion_factor
+                adc_light_value = adc_light.read_u16() * conversion_factor
+                print(
+                    f"Internal Temperature: {adc_int_temp_value:.2f} V ({int_temp_c:.2f} °C), Temperature: {adc_temp_value:.2f} V, Light: {adc_light_value:.2f} V"
+                )
 
-        post_data = json.dumps(
-            {"source": "pico-w", "text": "Connected to wifi", "ip": ip}
-        )
-        request_url = get_url_with_params(
-            REQUEST_URL,
-            {
-                "container_name": CONTAINER_NAME,
-                "topic_name": TOPIC_NAME,
-                "limit": LIMIT,
-            },
-        )
-        res = urequests.post(
-            request_url,
-            headers={"content-type": "application/json", "x-functions-key": AZURE_CODE},
-            data=post_data,
-        )
-        print(res.json())
-        res.close()
-
+                post_data = json.dumps(
+                    {
+                        "source": "pico-w",
+                        "text": "Connected to wifi",
+                        "ip": ip,
+                        "internal_temperature": f"{int_temp_c:.2f} degC",
+                        "internal_temperature_voltage": f"{adc_int_temp_value:.2f} V",
+                        "temperature_voltage": f"{adc_temp_value:.2f} V",
+                        "light_voltage": f"{adc_light_value:.2f} V",
+                    }
+                )
+                request_url = get_url_with_params(
+                    REQUEST_URL,
+                    {
+                        "container_name": CONTAINER_NAME,
+                        "topic_name": TOPIC_NAME,
+                        "limit": LIMIT,
+                    },
+                )
+                res = urequests.post(
+                    request_url,
+                    headers={
+                        "content-type": "application/json",
+                        "x-functions-key": AZURE_CODE,
+                    },
+                    data=post_data,
+                )
+                print(res.json())
+                res.close()
+                led.toggle()
+                sleep(SAMPLE_INTERVAL_SEC)  # sleep SAMPLE_INTERVAL_SEC sec
+        except KeyboardInterrupt:
+            print("Stopped by user.")
         wlan.disconnect()
     else:
         print(f"Error: Could not obtain a valid IP address. Status: {wlan.status()}")
